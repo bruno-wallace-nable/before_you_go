59fdd207f8a9e6b020264141652d27fa57dea850
Roberto de Farias <235542534+robertodefarias@users.noreply.github.com>
2026-03-18 20:20:29 -0300

    change routes back to lists and fix reports flow

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,12 +1,12 @@
 class ReportsController < ApplicationController
   def create
-    @place = Place.find(params[:place_id])
+    @place = Place.find(params[:list_id])
     @report = Report.new(report_params)
     @report.place = @place
     @report.user = current_user
 
     if @report.save
-      redirect_to place_path(@place), notice: "Report created."
+      redirect_to list_path(@place), notice: "Report created."
     else
       @reports = @place.reports
       render "places/show", status: :unprocessable_entity
